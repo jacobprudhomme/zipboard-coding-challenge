fb583d8e9beaa77c7ab37937711156f263b1164c
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2022-01-28 16:38:33 -0500

    Set up basic logged in/not logged in main page

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,22 +1,12 @@
 <script>
-  let counter = 0;
-  const addToCounter = () => {
-    counter += 1;
-  }
+  let loggedIn = false;
 </script>
 
 
-<div class="container">
-  <h1>Welcome to Meteor!</h1>
-
-  <button on:click={addToCounter}>Click Me</button>
-  <p>You've pressed the button {counter} times.</p>
-
-  <h2>Learn Meteor!</h2>
-  <ul>
-    <li><a href="https://svelte-tutorial.meteor.com/" target="_blank">Do the Tutorial</a></li>
-    <li><a href="http://guide.meteor.com" target="_blank">Follow the Guide</a></li>
-    <li><a href="https://docs.meteor.com" target="_blank">Read the Docs</a></li>
-    <li><a href="https://forums.meteor.com" target="_blank">Discussions</a></li>
-  </ul>
+<div>
+  {#if !loggedIn}
+    <p>Not logged in</p>
+  {:else}
+    <p>Already logged in</p>
+  {/if}
 </div>
