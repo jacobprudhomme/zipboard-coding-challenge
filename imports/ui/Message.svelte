c1e9b8eabf85a938dfa8632a1dc03f220f1ba01e
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2022-01-29 23:50:21 -0500

    Added time to post

--- imports/ui/Message.svelte
+++ imports/ui/Message.svelte
@@ -8,13 +8,22 @@
 
     return `${day < 10 ? '0' + day : day}/${month < 10 ? '0' + month : month}/${year}`;
   }
+
+  function formatTime(when) {
+    let hours = when.getHours();
+    const minutes = when.getMinutes();
+    const whichHalf = 0 <= hours && hours <= 11 ? 'AM' : 'PM';
+    hours = hours !== 0 && hours !== 12 ? hours % 12 : 12;
+
+    return `${hours}:${minutes < 10 ? '0' + minutes : minutes} ${whichHalf}`;
+  }
 </script>
 
 <div>
   <hr />
   <p><strong>{message.poster}</strong> said...</p>
   <p>{message.message}</p>
-  <p class="date">...on {formatDate(message.createdAt)}</p>
+  <p class="date">...on {formatDate(message.createdAt)} at {formatTime(message.createdAt)}</p>
 </div>
 
 <style>
